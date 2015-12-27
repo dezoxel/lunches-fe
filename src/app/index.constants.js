5aa50e1e9c501ce97d3db612f1c610fea931a678
dezoxel <dezoxel@gmail.com>
2015-12-28 00:28:43 +0200

    [Env] Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('lunchesFe')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
