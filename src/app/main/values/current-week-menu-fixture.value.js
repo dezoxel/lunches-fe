e655ec26b6ee85a6ff849c6fa4bbda5f60156bc8
dezoxel <dezoxel@gmail.com>
2016-01-11 16:20:25 +0200

    [DEZ-314] Covers added thru css

--- src/app/main/values/current-week-menu-fixture.value.js
+++ src/app/main/values/current-week-menu-fixture.value.js
@@ -93,6 +93,138 @@
             excluded: true
           }
         }
-      }
+      },
+      wed: {
+        alias: 'wed',
+        size: 'big',
+        price: {
+          big: {
+            meat_garnish_salad: 85,
+            meat_garnish: 75,
+            meat_salad: 70,
+            garnish_salad: 65,
+            meat: 55,
+            garnish: 50,
+            salad: 45
+          },
+          medium: {
+            meat_garnish_salad: 75,
+            meat_garnish: 65,
+            meat_salad: 60,
+            garnish_salad: 55,
+            meat: 45,
+            garnish: 40,
+            salad: 35
+          },
+        },
+        components: {
+          meat: {
+            title: 'Свиной биток',
+            weight: 240,
+            ingredients: ['Свинина'],
+            excluded: false
+          },
+          garnish: {
+            title: 'Рис с овощами',
+            weight: 300,
+            ingredients: ['Рис', 'Овощи'],
+            excluded: false
+          },
+          salad: {
+            title: 'Салат из свеклы с сыром',
+            weight: 100,
+            ingredients: ['Свекла, Сыр'],
+            excluded: true
+          }
+        }
+      },
+      thu: {
+        alias: 'thu',
+        size: 'big',
+        price: {
+          big: {
+            meat_garnish_salad: 85,
+            meat_garnish: 75,
+            meat_salad: 70,
+            garnish_salad: 65,
+            meat: 55,
+            garnish: 50,
+            salad: 45
+          },
+          medium: {
+            meat_garnish_salad: 75,
+            meat_garnish: 65,
+            meat_salad: 60,
+            garnish_salad: 55,
+            meat: 45,
+            garnish: 40,
+            salad: 35
+          },
+        },
+        components: {
+          meat: {
+            title: 'Свиной биток',
+            weight: 240,
+            ingredients: ['Свинина'],
+            excluded: false
+          },
+          garnish: {
+            title: 'Рис с овощами',
+            weight: 300,
+            ingredients: ['Рис', 'Овощи'],
+            excluded: false
+          },
+          salad: {
+            title: 'Салат из свеклы с сыром',
+            weight: 100,
+            ingredients: ['Свекла, Сыр'],
+            excluded: true
+          }
+        }
+      },
+      fri: {
+        alias: 'fri',
+        size: 'big',
+        price: {
+          big: {
+            meat_garnish_salad: 85,
+            meat_garnish: 75,
+            meat_salad: 70,
+            garnish_salad: 65,
+            meat: 55,
+            garnish: 50,
+            salad: 45
+          },
+          medium: {
+            meat_garnish_salad: 75,
+            meat_garnish: 65,
+            meat_salad: 60,
+            garnish_salad: 55,
+            meat: 45,
+            garnish: 40,
+            salad: 35
+          },
+        },
+        components: {
+          meat: {
+            title: 'Свиной биток',
+            weight: 240,
+            ingredients: ['Свинина'],
+            excluded: false
+          },
+          garnish: {
+            title: 'Рис с овощами',
+            weight: 300,
+            ingredients: ['Рис', 'Овощи'],
+            excluded: false
+          },
+          salad: {
+            title: 'Салат из свеклы с сыром',
+            weight: 100,
+            ingredients: ['Свекла, Сыр'],
+            excluded: true
+          }
+        }
+      },
     });
 })();
